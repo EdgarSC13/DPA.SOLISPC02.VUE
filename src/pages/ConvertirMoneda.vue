720b07389347526252e05532d8d6d78b4ef3d03f
EdgarSC <edgar13j13j13j@gmail.com>
2025-06-21 16:22:38 -0500

    Se agrego estilo al contenedor

--- src/pages/ConvertirMoneda.vue
+++ src/pages/ConvertirMoneda.vue
@@ -1,38 +1,30 @@
 <template>
-  <q-page padding>
-    <q-input
-      v-model="amount"
-      label="Monto"
-      type="number"
-      :rules="[(val) => val > 0 || 'Debe ser un monto válido']"
-      lazy-rules
-    />
-    <q-select
-      v-model="fromCurrency"
-      :options="currencies"
-      label="Moneda de origen"
-      emit-value
-      map-options
-    />
-    <q-select
-      v-model="toCurrency"
-      :options="currencies"
-      label="Moneda de destino"
-      emit-value
-      map-options
-    />
-    <q-btn label="Convertir" @click="convertCurrency" :disabled="isLoading" />
+  <div class="contenedor">
+    <h6 class="Titulo">Conversión de Monedas</h6>
+    <q-page padding>
+      <q-input
+        v-model="amount"
+        label="Monto a convertir"
+        type="number"
+        :rules="[(val) => val > 0 || 'Debe ser un monto válido']"
+        lazy-rules
+      />
+      <q-select v-model="fromCurrency" :options="currencies" label="Desde" emit-value map-options />
+      <q-select v-model="toCurrency" :options="currencies" label="Hacia" emit-value map-options />
 
-    <q-btn label="Intercambiar Monedas" @click="swapCurrencies" />
+      <div class="boton-container">
+        <q-btn label="Convertir" @click="convertCurrency" :disabled="isLoading" color="primary" />
+        <q-btn label="Intercambiar Monedas" @click="swapCurrencies" color="purple" />
+      </div>
+      <q-banner v-if="conversionResult" class="q-mt-md">
+        {{ amount }} {{ fromCurrency }} equivalen a {{ conversionResult }} {{ toCurrency }}
+      </q-banner>
 
-    <q-banner v-if="conversionResult" class="q-mt-md">
-      {{ amount }} {{ fromCurrency }} equivalen a {{ conversionResult }} {{ toCurrency }}
-    </q-banner>
-
-    <q-banner v-if="errorMessage" class="q-mt-md" color="negative">
-      {{ errorMessage }}
-    </q-banner>
-  </q-page>
+      <q-banner v-if="errorMessage" class="q-mt-md" color="negative">
+        {{ errorMessage }}
+      </q-banner>
+    </q-page>
+  </div>
 </template>
 
 <script>
@@ -117,5 +109,23 @@ export default {
 .q-banner {
   font-size: 18px;
   text-align: center;
+}
+
+.contenedor {
+  width: 50%;
+  height: 80vh; /* Ocupar el 80% de la altura de la pantalla (opcional) */
+  margin: 30px auto 0;
+  padding: 20px;
+  border: 2px solid #2c0672; /* Borde de 2px de grosor y color negro */
+  border-radius: 10px; /* Bordes redondeados (opcional, puedes ajustarlo) */
+  box-sizing: border-box;
+}
+
+.boton-container {
+  text-align: center; /* Centra los botones */
+}
+
+.boton-container button {
+  margin: 10px; /* Espacio entre los botones */
 }
 </style>
